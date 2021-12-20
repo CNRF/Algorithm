34d3e15c27f27773d89ae003ae8a190e4a4d87bb
CNRF <834031172@qq.com>
2021-12-20 10:04:36 +0800

    feature: 快速排序
    快速排序：比基准数小或等于的放左边

---
+++ src/code/Code25_QuickSort.java
@@ -0,0 +1,136 @@
+package code;
+
+/**
+ * @author wmx
+ * @version 1.0
+ * @className Code25_QuickSort
+ * @description 数组的快速排序
+ * @date 2021/12/14 11:30
+ */
+public class Code25_QuickSort {
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    //设置数组的基准值
+
+    // arr[L..R]上，以arr[R]位置的数做划分值
+    // <= X > X
+    // <= X X
+    public static int partition(int[] arr, int L, int R) {
+        if (L > R) {
+            return -1;
+        }
+        if (L == R) {
+            return L;
+        }
+        int lessEqual = L - 1;
+        int index = L;
+        while (index < R) {
+            if (arr[index] <= arr[R]) {
+                swap(arr, index, ++lessEqual);
+            }
+            index++;
+        }
+        swap(arr, ++lessEqual, R);
+        return lessEqual;
+    }
+
+    /**
+     * 快速排序的第一种实现方式，大于目标放左边，小于等于放右边
+     *
+     * @param arr
+     */
+    public static void quickSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process1(arr, 0, arr.length - 1);
+    }
+
+    public static void process1(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        //在此处获取到原数组中R放在新排序位置
+        // [ <=arr[R]     arr[R]      >arr[R] ]
+        //在新确定的位置递归即可
+        int M = partition(arr, L, R);
+        process1(arr, L, M - 1);
+        process1(arr, M + 1, R);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            int[] arr3 = copyArray(arr1);
+            quickSort1(arr1);
+            ArrayUtils.comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Oops!");
+
+    }
+}
