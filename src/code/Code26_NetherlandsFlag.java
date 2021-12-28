9a43772207aa86d094f65d963f795b58c2dfd8f5
wmx <834031172@qq.com>
2021-12-28 09:21:55 +0800

    feature: 快速排序（荷兰国旗问题）
    快速排序：比基准数小或等于的放左边

--- src/code/Code26_NetherlandsFlag.java
+++ src/code/Code26_NetherlandsFlag.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.util.Stack;
+
 /**
  * @author wmx
  * @version 1.0
@@ -58,8 +60,114 @@ public class Code26_NetherlandsFlag {
         process2(arr, equalArea[1] + 1, R);
     }
 
-    public static void main(String[] args) {
+    // 快排3.0 非递归版本
+    public static void quickSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        swap(arr, (int) (Math.random() * N), N - 1);
+        int[] equalArea = netherlandsFlag(arr, 0, N - 1);
+        int el = equalArea[0];
+        int er = equalArea[1];
+        Stack<Op> stack = new Stack<>();
+        stack.push(new Op(0, el - 1));
+        stack.push(new Op(er + 1, N - 1));
+        while (!stack.isEmpty()) {
+            Op op = stack.pop(); // op.l  ... op.r
+            if (op.l < op.r) {
+                swap(arr, op.l + (int) (Math.random() * (op.r - op.l + 1)), op.r);
+                equalArea = netherlandsFlag(arr, op.l, op.r);
+                el = equalArea[0];
+                er = equalArea[1];
+                stack.push(new Op(op.l, el - 1));
+                stack.push(new Op(er + 1, op.r));
+            }
+        }
+    }
 
+    // 生成随机数组（用于测试）
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // 拷贝数组（用于测试）
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // 对比两个数组（用于测试）
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 打印数组（用于测试）
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            quickSort1(arr1);
+            quickSort2(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println("test end");
+        System.out.println("测试" + testTime + "组是否全部通过：" + (succeed ? "是" : "否"));
+
+    }
+
+    // 快排非递归版本需要的辅助类
+    // 要处理的是什么范围上的排序
+    public static class Op {
+        public int l;
+        public int r;
+
+        public Op(int left, int right) {
+            l = left;
+            r = right;
+        }
     }
 
 }
