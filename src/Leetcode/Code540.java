4c520461bab30a567e582332313bc6ed2c732c35
mark.wang <834031172@qq.com>
2022-02-15 16:10:06 +0800

    feature: leetcode 540解

--- src/Leetcode/Code540.java
+++ src/Leetcode/Code540.java
@@ -10,15 +10,45 @@ package Leetcode;
 public class Code540 {
     /**
      * 使用异或（1^1=0）直接获取相关数据
-     * @param nums
-     * @return
+     *
+     * @param nums 数组
+     * @return 返回唯一出现奇数次的数
      */
     public int singleNonDuplicate(int[] nums) {
 
-        int num=0;
+        int num = 0;
         for (int i : nums) {
-            num=num^i;
+            num = num ^ i;
         }
         return num;
+    }
+
+    /**
+     * 使用二分法查找相关数据
+     *
+     * @param nums 数组
+     * @return 返回只出现一次的数
+     */
+    public int singleNonDuplicate2(int[] nums) {
+        int length = nums.length;
+        int l = 0;
+        int r = length - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (mid % 2 == 0) {
+                if (mid + 1 < length && nums[mid] == nums[mid + 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            } else {
+                if (mid - 1 >= 0 && nums[mid - 1] == nums[mid]) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+        }
+        return nums[r];
     }
 }
